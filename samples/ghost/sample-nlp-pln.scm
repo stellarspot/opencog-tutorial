6232027f3a1b7287e8966ebb6d504fd3431bdfcf
Alexander Scherbatiy <stellarspot@gmail.com>
2019-04-09 15:56:57 +0300

    Add NLP PLN sample

---
+++ samples/ghost/sample-nlp-pln.scm
@@ -0,0 +1,27 @@
+(use-modules
+ (opencog)
+ (opencog nlp)
+ (opencog nlp chatbot)
+ (opencog nlp relex2logic)
+ (opencog ghost)
+ (opencog ghost procedures)
+ (srfi srfi-1))
+
+; Load rule-base for the trail of inference
+(load-trail-3)
+
+(nlp-parse "dogs can bark")
+(nlp-parse "Tobby is a dog")
+
+(do_pln)
+
+; Utility for testing as sureal part is broken
+(define (get-atoms-for-sureal trail)
+ (define pln-outputs (cog-value->list
+                      (cog-value trail (Predicate "inference-results"))))
+ (if (null? pln-outputs) '() (cog-outgoing-set (filter-for-sureal pln-outputs))))
+
+
+(display
+ (get-atoms-for-sureal rb-trail-3))
+
