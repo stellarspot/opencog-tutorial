23db8aa6b49e537f7d2e3f684411697a9d471edb
Alexander Scherbatiy <stellarspot@gmail.com>
2019-04-10 13:56:48 +0300

    USe method call in Ghost sample

--- samples/ghost/sample-ghost-zoo.scm
+++ samples/ghost/sample-ghost-zoo.scm
@@ -6,19 +6,30 @@
  (opencog ghost procedures)
 )
 
+
+; Ghost methods
+
+(define-public (call_animal animal) (List (Word (cog-name animal)) (Word (cog-name animal)) (Word (cog-name animal))))
+
 ; Ghost Rules
 (ghost-parse "
 concept: ~bird [flamingo swan]
 concept: ~reptile [turtle snake]
+concept: ~mammal [lion leopard elephant]
+concept: ~animal [~bird ~reptile ~mammal]
+concept: ~dangerous_animal [lion snake]
+
 concept: ~attitude [like hate]
 
-r: ([hello hi]) Welcome to the Zoo! Which animals do you want to see?
+p: Welcome to the Zoo! Which animals do you want to see?
   j1: (see ~bird) Birds are so beautiful. Let's go to see them!
   j1: (see ~reptile) Good choice. Let's go to see reptiles!
 
 r: (Do you ~attitude _~bird) $animal=_0 I am a fun of $animal!
 
 r: (Do you ~attitude _~reptile) $animal=_0 I am afraid of $animal!
+
+r: (what is amusing _~animal) $animal=_0 Just call it $animal - ^call_animal($animal)!
 
 ")
 
@@ -31,3 +42,4 @@ r: (Do you ~attitude _~reptile) $animal=_0 I am afraid of $animal!
 
 (test-ghost "Do you like swans?")
 (test-ghost "Do you hate snakes?")
+(test-ghost "What is amusing elephant!")
