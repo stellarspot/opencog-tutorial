b0c6c947338aa769e4e2055a79979f1d81243c71
Alexander Scherbatiy <stellarspot@gmail.com>
2019-04-24 12:36:44 +0000

    Parse I work in ... in Ghost notebook sample

--- samples/ghost/sample-ghost-notebook.scm
+++ samples/ghost/sample-ghost-notebook.scm
@@ -5,9 +5,7 @@
  (opencog ghost)
  (opencog ghost procedures)
 
- ; Python bindings
  (opencog exec)
- (opencog python)
 )
 
 ; Legend 1:
@@ -16,35 +14,93 @@
 ; A: You work in SoftMegaCorp.
 
 ; Legend 2:
+; Alice work in HardMegaCorp.
+; Q: Where does Alice work?
+; A: Alice works in HardMegaCorp.
+
+; Legend 3:
 ; I work in SoftMegaCorp.
 ; Alice works in HardMegaCorp.
 ; Bob is my colegue.
 ; Q: Where does Bob work?
 ; A: Bob work in SoftMegaCorp.
 
-; Python methods
+; Parse facts rules
 
-(python-eval "
+(define parse-facts-work-rule
+ (Bind
+  (VariableList
+   ; verb
+   (TypedVariable
+    (Variable "$verb_inst")
+    (Type "PredicateNode"))
+   ; who
+   (TypedVariable
+    (Variable "$who_inst")
+    (Type "ConceptNode"))
+   (TypedVariable
+    (Variable "$who")
+    (Type "ConceptNode"))
+   ; preposition
+   (TypedVariable
+    (Variable "$preposition_inst")
+    (Type "PredicateNode"))
+   ; where
+   (TypedVariable
+    (Variable "$where_inst")
+    (Type "ConceptNode"))
+   (TypedVariable
+    (Variable "$where")
+    (Type "ConceptNode")))
+  (And
+   (ImplicationLink
+    (Variable "$verb_inst")
+    (PredicateNode "work"))
+   (InheritanceLink
+    (Variable "$who_inst")
+    (Variable "$who"))
+   (InheritanceLink
+    (Variable "$where_inst")
+    (Variable "$where"))
+   (EvaluationLink
+    (Variable "$verb_inst")
+    (ListLink
+     (Variable "$who_inst")))
+   (EvaluationLink
+    (Variable "$preposition_inst")
+    (ListLink
+     (Variable "$verb_inst")
+     (Variable "$where_inst"))))
+  (Evaluation
+   (Predicate "work")
+   (List
+    (Variable "$who")
+    (Variable "$where")))))
 
-from opencog.atomspace import types
-from opencog.type_constructors import *
-from opencog.scheme_wrapper import scheme_eval_as
-from opencog.cogserver_type_constructors import *
-
-atomspace = scheme_eval_as('(cog-atomspace)')
-set_type_ctor_atomspace(atomspace)
-
-")
 
 ; Ghost methods
+
+(define-public
+ (parse-facts)
+ (cog-execute! parse-facts-work-rule)
+ (List))
 
 ;; Ghost Rules
 (ghost-parse "
 
 p: This is your personal Notebook assistant
+
+r: (I work in _*)
+  '_0 is a great company!
+  ^parse-facts()
+  ^keep()
+
+r: (where do _* work) Just try to find where '_0 work.
+  ^keep()
 ")
 
 ; Test Ghost
 (test-ghost "I work in SoftMegaCorp.")
+(test-ghost "Where do I work?")
 
 ;(cog-prt-atomspace)
