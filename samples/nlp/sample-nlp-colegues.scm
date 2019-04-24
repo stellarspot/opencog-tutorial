2ee5f81a5fcd6dfba0f99b3e7978084637890493
Alexander Scherbatiy <stellarspot@gmail.com>
2019-04-24 10:16:06 +0000

    Parse where in NLP colegues sample

--- samples/nlp/sample-nlp-colegues.scm
+++ samples/nlp/sample-nlp-colegues.scm
@@ -8,14 +8,27 @@
 (define work-rule
  (Bind
   (VariableList
+   ; verb
    (TypedVariable
     (Variable "$verb_inst")
     (Type "PredicateNode"))
+   ; who
    (TypedVariable
     (Variable "$who_inst")
     (Type "ConceptNode"))
    (TypedVariable
     (Variable "$who")
+    (Type "ConceptNode"))
+   ; preposition
+   (TypedVariable
+    (Variable "$preposition_inst")
+    (Type "PredicateNode"))
+   ; where
+   (TypedVariable
+    (Variable "$where_inst")
+    (Type "ConceptNode"))
+   (TypedVariable
+    (Variable "$where")
     (Type "ConceptNode")))
   (And
    (ImplicationLink
@@ -24,14 +37,23 @@
    (InheritanceLink
     (Variable "$who_inst")
     (Variable "$who"))
+   (InheritanceLink
+    (Variable "$where_inst")
+    (Variable "$where"))
    (EvaluationLink
     (Variable "$verb_inst")
     (ListLink
-     (Variable "$who_inst"))))
+     (Variable "$who_inst")))
+   (EvaluationLink
+    (Variable "$preposition_inst")
+    (ListLink
+     (Variable "$verb_inst")
+     (Variable "$where_inst"))))
   (Evaluation
    (Predicate "work")
    (List
-    (Variable "$who")))))
+    (Variable "$who")
+    (Variable "$where")))))
 
 
 (nlp-parse "I work in SoftMegaCorp.")
