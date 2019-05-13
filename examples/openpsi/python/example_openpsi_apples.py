be12a75a4d2767395b5efeb43caaadca33ac026a
Alexander Scherbatiy <stellarspot@gmail.com>
2019-05-13 14:28:21 +0000

    Update OpenPsi eat apples example

--- examples/openpsi/python/example_openpsi_apples.py
+++ examples/openpsi/python/example_openpsi_apples.py
@@ -11,90 +11,97 @@ APPLE = ConceptNode("apple")
 
 # Called by OpenPsi Action
 def eat_apple(apple):
-    print("[openpsi] eat apple")
-    print(apple)
-    ConceptNode("finished")
+    print("[openpsi] eat apple:", apple.name)
+    # Mark apple as handled
+    InheritanceLink(apple, ConceptNode("handled"))
+    return ConceptNode("finished")
 
 
-# # OpenPsi context
-# def openpsi_context():
-#     return AndLink(
-#         InheritanceLink(
-#             VariableNode("$APPLE"),
-#             APPLE))
-#
-#
-# # Pass OpenPsi context to Scheme
-# EvaluationLink(
-#     PredicateNode("openpsi-context"),
-#     openpsi_context())
-#
+# OpenPsi context
+def openpsi_context():
+    return AndLink(
+        InheritanceLink(
+            VariableNode("$APPLE"),
+            APPLE),
+        AbsentLink(
+            InheritanceLink(
+                VariableNode("$APPLE"),
+                ConceptNode("handled"))))
+
+
+# Pass OpenPsi context to Scheme
+EvaluationLink(
+    PredicateNode("openpsi-context"),
+    openpsi_context())
 
 # Call Scheme
 from opencog.scheme_wrapper import scheme_eval
 
 scheme_eval(atomspace,
             '''
-(use-modules
- (opencog)
- (opencog openpsi))
+            (use-modules
+             (opencog)
+             (opencog exec)
+             (opencog openpsi))
 
-; OpenPsi sample
-; Eat apples
+            ; OpenPsi sample
+            ; Eat apples
 
-; OpenPsi Goal: eat apples
-(define goal
- (Concept "goal-eat-apples"))
+            ; OpenPsi Goal: eat apples
+            (define goal
+             (Concept "goal-eat-apples"))
 
-(define context
- (list
-  (Inheritance (Variable "$APPLE") (Concept "apple"))))
+            ; OpenPsi Context retrieved from Python
+            (define context
+             (cog-outgoing-set
+              (car
+               (cog-outgoing-set
+                (cog-execute!
+                 (Get
+                  (Evaluation
+                   (Predicate "openpsi-context")
+                   (Variable "$CONTEXT"))))))))
 
-; OpenPsi Action: eat apple
-(define action
- (ExecutionOutput
-  (GroundedSchema "scm: eat-apple")
-  ;(GroundedSchema "py: eat_apple")
-  (List
-   (Variable "$APPLE")
-  )))
 
-(define (eat-apple apple)
- (display "[openpsi] eat apple action\n")
- (display apple)
- (ConceptNode "finished"))
+            ; OpenPsi Action: eat apple
+            ; Call Python method implementation
+            (define action
+             (ExecutionOutput
+              (GroundedSchema "py: eat_apple")
+              (List
+               (Variable "$APPLE"))))
 
-; OpenPsi component
-(define component (psi-component "component"))
+            ; OpenPsi component
+            (define component (psi-component "component"))
 
-; OpenPsi Rule: Eat apple
-(define rule
- (psi-rule
-  context
-  action
-  goal
-  (stv 1 1)
-  component))
+            ; OpenPsi Rule: Eat apple
+            (define rule
+             (psi-rule
+              context
+              action
+              goal
+              (stv 1 1)
+              component))
 
 
             ; Run OpenPsi
             (psi-run component)
-
             ''')
 
 delay = 0.2
 
 InheritanceLink(ConceptNode("apple-1"), APPLE)
+InheritanceLink(ConceptNode("apple-2"), APPLE)
 
 time.sleep(delay)
 
-InheritanceLink(ConceptNode("apple-2"), APPLE)
+InheritanceLink(ConceptNode("apple-3"), APPLE)
+InheritanceLink(ConceptNode("apple-4"), APPLE)
 
 time.sleep(delay)
 
 scheme_eval(atomspace,
             '''
-
             ; Stop OpenPsi
             (psi-halt component)
-           ''')
+            ''')
