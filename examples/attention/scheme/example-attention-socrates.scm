130418c77a5bb58efbae930397c6054ef7937b33
Alexander Scherbatiy <stellarspot@gmail.com>
2019-06-18 13:52:55 +0300

    Add Attention Focus Scheme example

---
+++ examples/attention/scheme/example-attention-socrates.scm
@@ -0,0 +1,43 @@
+(use-modules (opencog) (opencog exec))
+(use-modules (opencog attention-bank))
+
+; Socrates, Einstein, and Peirce are all men.
+(cog-set-av! (ConceptNode "Socrates" (stv .001 1)) (cog-new-av 15 10 0))
+(cog-set-av! (ConceptNode "Einstein" (stv .001 1)) (cog-new-av 45 10 0))
+(cog-set-av! (ConceptNode "Peirce" (stv .001 1)) (cog-new-av 15 10 0))
+(cog-set-av! (ConceptNode "man" (stv .01 1)) (cog-new-av 35 10 0))
+
+
+(InheritanceLink (stv 1.0 1.0)
+ (ConceptNode "Socrates")
+ (ConceptNode "man") )
+
+(cog-set-av!
+ (InheritanceLink (stv 1.0 1.0)
+  (ConceptNode "Einstein")
+  (ConceptNode "man")) (cog-new-av 35 10 0))
+
+(InheritanceLink (stv 1.0 1.0)
+ (ConceptNode "Peirce")
+ (ConceptNode "man"))
+
+; Pattern for the pattern matcher to match, to find them.
+(define find-man
+ (BindLink
+  (VariableNode "$X")
+  (InheritanceLink
+   (VariableNode "$X")
+   (ConceptNode "man"))
+  (VariableNode "$X")))
+
+(sleep 2)
+
+(display "Bind with Attention Focus:\n")
+(display
+ (cog-bind-af find-man)
+)
+
+(display "Bind without Attention Focus:\n")
+(display
+ (cog-execute! find-man)
+)
